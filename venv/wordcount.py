9db0aa714bbc5e012e9083574af83f6058718296
Gregory <g.zaugg97@gmail.com>
2019-08-21 20:44:33 +0200

    Added filepath explorer

--- venv/wordcount.py
+++ venv/wordcount.py
@@ -1,33 +1,19 @@
 import time
 import curses
 import curses.ascii
+import tkinter
+from tkinter import filedialog
 
 stdscr = curses.initscr()
 
 curses.start_color()
 curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-def navKey(key):
-
-    if key == curses.KEY_UP:
-        return "UP"
-    elif key == curses.KEY_DOWN:
-        return "DOWN"
-    elif key == curses.KEY_LEFT:
-        return "LEFT"
-    elif key == curses.KEY_RIGHT:
-        return "RIGHT"
-    elif key == curses.KEY_ENTER or key in [10, 13]:
-        return "ENTER"
-    elif key == curses.ascii.ESC:
-        return "ESC"
-
-
-def inputGoal(inputString, stdscr):
+def input_goal(input_string, stdscr):
 
     try:
-      inputInt = int(inputString)
-      return inputInt
+      input_int = int(input_string)
+      return input_int
     except:
         stdscr.clear()
         stdscr.addstr(1, 4, "That is not a number you Dimwit...")
@@ -45,7 +31,7 @@ def inputGoal(inputString, stdscr):
         while 1:
             key = stdscr.getch()
 
-            if navKey(key) == "ENTER":
+            if key == curses.KEY_ENTER or key in [10, 13]:
                 break
 
         return -1
@@ -55,7 +41,7 @@ def inputGoal(inputString, stdscr):
         stdscr.refresh()
 
 
-def startScreen(stdscr):
+def start_screen(stdscr):
 
 
     stdscr.clear()
@@ -66,15 +52,33 @@ def startScreen(stdscr):
     curses.echo()
     curses.curs_set(1)
 
-    goalStringRaw = str(stdscr.getstr(3,4))
-    goalString = goalStringRaw[2 : (len(goalStringRaw)-1)]
+    goal_string_raw = str(stdscr.getstr(3,4))
+    goal_string = goal_string_raw[2 : (len(goal_string_raw)-1)]
 
     curses.noecho()
     curses.curs_set(0)
 
-    goalInt = inputGoal(goalString, stdscr)
+    goal_int = input_goal(goal_string, stdscr)
 
-    return goalInt
+    return goal_int
+
+def file_select(stdscr, selected_line_idx):
+    menu = ["Select file", "Change goal"]
+
+    stdscr.clear
+
+    for idx, line in enumerate(menu):
+        y = 3
+        x = 7 + ((idx + 1) ** 5)
+
+        if idx == selected_line_idx:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y, x, line)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, line)
+
+    stdscr.refresh()
 
 def main(stdscr):
 
@@ -84,10 +88,50 @@ def main(stdscr):
 
     goal = -1
 
-    while goal < 0:
-        goal = startScreen(stdscr)
+    while 1:
+        goal = start_screen(stdscr)
+        if goal < 0:
+            continue
 
-    stdscr.addstr(1, 4, "Your Goal was set to: %d" % goal)
+        stdscr.addstr(1, 4, "Please select file to track:")
+
+        current_line_idx = 0
+
+        file_select(stdscr, current_line_idx)
+
+        while 1:
+            key = stdscr.getch()
+
+            stdscr.clear()
+
+            if key == curses.KEY_LEFT and current_line_idx > 0:
+                current_line_idx -= 1
+            elif key == curses.KEY_RIGHT and current_line_idx < 1:
+                current_line_idx += 1
+            elif key == curses.KEY_ENTER or key in [10, 13]:
+                break
+
+            stdscr.addstr(1, 4, "Please select file to track:")
+
+            file_select(stdscr, current_line_idx)
+
+            stdscr.refresh()
+
+        if current_line_idx == 0:
+            break
+        elif current_line_idx == 1:
+            continue
+
+
+
+    root = tkinter.Tk()
+    root.withdraw()
+
+    filepath = filedialog.askopenfilename()
+
+
+
+    stdscr.addstr(1, 4, "Your Goal was set to: %d and your filepath is %s" % (goal, filepath))
     stdscr.refresh()
     time.sleep(5)
 
